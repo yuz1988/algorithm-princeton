3d0f8bebfe4fc6d044cea792b20833332918b930
炒菜王 <yuz1988@iastate.edu>
2016-11-17 15:32:46 -0600

    percolation

---
+++ algorithm-princeton/src/Percolation.java
@@ -0,0 +1,101 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+	
+	private int n;
+	private boolean[][] matrix;
+	private WeightedQuickUnionUF uFinder;
+	private WeightedQuickUnionUF uFinderTop;
+	
+	// create n-by-n grid, with all sites blocked
+	public Percolation(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		else {
+			this.n = n;
+			matrix = new boolean[n][n];
+			// two virtual positions: beginning and end
+			uFinder = new WeightedQuickUnionUF(n*n + 2);
+			
+			// for "backwash" problem, only create top virtual position
+			uFinderTop = new WeightedQuickUnionUF(n*n + 1);
+		}
+	}
+
+	// open site (row, col) if it is not open already
+	public void open(int row, int col) {
+		row--;
+		col--;
+		if (row < 0 || row >= n || col < 0 || col >= n) {
+			throw new IndexOutOfBoundsException();
+		}
+		else if (!matrix[row][col]) {
+			matrix[row][col] = true;
+			
+			int index = row*n + col + 1;
+			// up
+			if (row == 0) {
+				uFinder.union(0, index);
+				uFinderTop.union(0, index);
+			}
+			else if (matrix[row-1][col]) {
+				uFinder.union(index, index-n);
+				uFinderTop.union(index, index-n);
+			}
+			
+			// down
+			if (row == n-1) {
+				uFinder.union(n*n+1, index);
+			}
+			else if (matrix[row+1][col]) {
+				uFinder.union(index, index+n);
+				uFinderTop.union(index, index+n);
+			}
+			
+			// left
+			if (col > 0 && matrix[row][col-1]) {
+				uFinder.union(index, index-1);
+				uFinderTop.union(index, index-1);
+			}
+			
+			// right
+			if (col < n-1 && matrix[row][col+1]) {
+				uFinder.union(index, index+1);
+				uFinderTop.union(index, index+1);
+			}
+		}
+	}
+
+	// is site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		row--;
+		col--;
+		if (row < 0 || row >= n || col < 0 || col >= n) {
+			throw new IndexOutOfBoundsException();
+		}
+		else {
+			return matrix[row][col];
+		}
+	}
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (isOpen(row, col)) {
+            row--;
+            col--;
+            return uFinderTop.connected(0, row * n + col + 1);
+        }
+        return false;
+	}
+	
+	// does the system percolate?
+	public boolean percolates() {
+		return uFinder.connected(0, n*n+1);
+	}
+
+	// test client (optional)
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ algorithm-princeton/src/PercolationStats.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+	
+	private int trials;
+	private double[] fractions;
+	
+	// perform trials independent experiments on an n-by-n grid
+	public PercolationStats(int n, int trials) {
+		if (n <= 0 || trials <= 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		this.trials = trials;
+		fractions = new double[trials];
+		
+		// repeat experiment @trials times
+		for (int i = 0; i < trials; i++) {
+			int count = 0;
+			Percolation p = new Percolation(n);
+			while (!p.percolates()) {
+				int row = StdRandom.uniform(n) + 1;
+				int col = StdRandom.uniform(n) + 1;
+				if (!p.isOpen(row, col)) {
+					p.open(row, col);
+					count++;
+				}
+			}
+			fractions[i] = ((double) count) / (n*n);
+		}
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+		return StdStats.mean(fractions);
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return StdStats.stddev(fractions);
+	}
+
+	// low  endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return mean()-1.96*stddev()/Math.sqrt(trials);
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return mean()+1.96*stddev()/Math.sqrt(trials);
+	}
+
+	// test client (described below)
+	public static void main(String[] args) {
+		PercolationStats ps = new PercolationStats(2, 100);
+		System.out.println(ps.mean());
+		System.out.println(ps.stddev());
+		System.out.println(ps.confidenceLo() + "   " + ps.confidenceHi());
+	}
+}
